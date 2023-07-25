094cbb5922a7dc9102a4d9e142b9bad5a5c59ae2
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-07-25 22:24:12 +0100

    3.2 phonebook backend step 2 finished

--- index.js
+++ index.js
@@ -24,11 +24,16 @@ let persons = [
   },
 ];
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-const PORT = 3001
-app.listen(PORT, ()=> {
-  console.log(`Server running on port ${PORT}`)
-})
+app.get("/info", (request, response) => {
+  response.send(`<p>phonebook has info for ${persons.length} people</p> <p>${new Date()} (${Intl.DateTimeFormat().resolvedOptions().timeZone})</p>
+  `);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
